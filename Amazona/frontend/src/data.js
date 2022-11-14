d40a2177ad422291819a6d326ad9d459a59df46c
Thuli <nthulitha@gmail.com>
2022-11-15 01:08:08 +0530

    1

---
+++ Amazona/frontend/src/data.js
@@ -0,0 +1,71 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Red Long Frock',
+            category: 'Frocks',
+            image: './images/p1.png',
+            price: 120,
+            brand: 'Kasandra',
+            rating:4.5,
+            numReviews: 10,
+            description:'High quality material'
+        },
+        {
+            _id: '2',
+            name: 'Black Trousers & Jackets',
+            category: 'Casuals',
+            image: './images/p2.png',
+            price: 150,
+            brand: 'Ariyana',
+            rating:3.5,
+            numReviews: 15,
+            description:'High quality material'
+        },
+        {
+            _id: '3',
+            name: 'Green Color Blazers',
+            category: 'Blazers',
+            image: './images/p3.png',
+            price: 500,
+            brand: 'Trenty',
+            rating:5.0,
+            numReviews: 20,
+            description:'High quality material'
+        },
+        {
+            _id: '4',
+            name: 'Purple Color Jackets',
+            category: 'Casuals',
+            image: './images/p4.png',
+            price: 240,
+            brand: 'Ariyana',
+            rating:4.5,
+            numReviews: 15,
+            description:'High quality material'
+        },
+        {
+            _id: '5',
+            name: 'Red Color T-Shirts',
+            category: 'T-Shirts',
+            image: './images/p5.png',
+            price: 100,
+            brand: 'Blizare',
+            rating:4.5,
+            numReviews: 16,
+            description:'High quality material'
+        },
+        {
+            _id: '6',
+            name: 'White Shirt & Denim',
+            category: 'Casuals',
+            image: './images/p6.png',
+            price: 400,
+            brand: 'Virtuza',
+            rating:3.5,
+            numReviews: 10,
+            description:'High quality material'
+        }
+    ]
+}
+export default data;
